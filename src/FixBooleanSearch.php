2b3954fe5edb7f6d406a33537222e5892d38348d
Nicolaas <github@sunnysideup.co.nz>
2021-03-23 17:41:00 +1300

    WIP

--- src/FixBooleanSearch.php
+++ src/FixBooleanSearch.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Sunnysideup\YesNoAnyFilter;
 
 use SilverStripe\Forms\DropdownField;
@@ -7,24 +8,23 @@ trait FixBooleanSearch
 {
     public function scaffoldSearchFields($_params = null)
     {
-
         $fields = parent::scaffoldSearchFields($_params);
 
         $anyText = _t('SilverStripe\\ORM\\FieldType\\DBBoolean.ANY', '-- Any --');
         $source = [
             null => _t('SilverStripe\\ORM\\FieldType\\DBBoolean.ANY', '-- Any --'),
             1 => _t('SilverStripe\\ORM\\FieldType\\DBBoolean.YESANSWER', 'Yes'),
-            0 => _t('SilverStripe\\ORM\\FieldType\\DBBoolean.NOANSWER', 'No')
+            0 => _t('SilverStripe\\ORM\\FieldType\\DBBoolean.NOANSWER', 'No'),
         ];
         $searchableFields = $this->searchableFields();
-        if(count($searchableFields)) {
+        if (count($searchableFields)) {
             $dbs = $this->Config()->get('db');
-            foreach($searchableFields as $fieldName => $spec) {
+            foreach ($searchableFields as $fieldName => $spec) {
                 $fieldTitle = $spec['title'] ?? $fieldName;
                 $type = $dbs[$fieldName] ?? 'error';
-                if( stripos($type, 'Boolean') === 0) {
+                if (stripos($type, 'Boolean') === 0) {
                     $field = new DropdownField($fieldName, $fieldTitle, $source);
-                    $field->setEmptyString('-- Any --');
+                    $field->setEmptyString($anyText);
                     $fields->replaceField($fieldName, $field);
                 }
             }
